5c40bd70e7c3bc7f64f7f1d3da3348f4197c0a05
WindomZ <git.windomz@gmail.com>
2017-06-15 17:31:05 +0800

    update(sql): add coupon_batch

--- sql/coupondb.sql
+++ sql/coupondb.sql
@@ -2,6 +2,33 @@ DROP SCHEMA IF EXISTS coupondb;
 
 CREATE SCHEMA coupondb CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+create table coupondb.cp_coupon
+(
+	id char(36) not null,
+	post_time datetime default CURRENT_TIMESTAMP not null,
+	put_time datetime default CURRENT_TIMESTAMP not null,
+	name varchar(32) default '' not null,
+	active tinyint(1) default '1' not null,
+	level int default '0' not null,
+	`desc` varchar(256) default '' not null,
+	owner_id char(36) not null,
+	activity_id char(36) not null,
+	template_id char(36) not null,
+	pack_id char(36) not null,
+	batch_id char(36) not null,
+	class int default '0' not null,
+	kind int default '0' not null,
+	product_id varchar(36) default '' not null,
+	min_amount decimal(10,2) default '0.00' not null,
+	offer_amount decimal(10,2) default '0.00' not null,
+	used_count int default '0' not null,
+	used_time datetime default CURRENT_TIMESTAMP not null,
+	dead_time datetime default CURRENT_TIMESTAMP not null,
+	constraint cp_coupon_id_uindex
+		unique (id)
+)
+;
+
 create table coupondb.cp_coupon_activity
 (
 	id char(36) not null,
@@ -23,7 +50,21 @@ create table coupondb.cp_coupon_activity
 )
 ;
 
-create table coupondb.cp_coupon
+create table coupondb.cp_coupon_batch
+(
+	id char(36) not null,
+	post_time datetime default CURRENT_TIMESTAMP not null,
+	put_time datetime default CURRENT_TIMESTAMP not null,
+	owner_id char(36) not null,
+	activity_id char(36) not null,
+	template_id char(36) not null,
+	pack_id char(36) not null,
+	constraint cp_coupon_batch_id_uindex
+		unique (id)
+)
+;
+
+create table coupondb.cp_coupon_pack
 (
 	id char(36) not null,
 	post_time datetime default CURRENT_TIMESTAMP not null,
@@ -32,18 +73,10 @@ create table coupondb.cp_coupon
 	active tinyint(1) default '1' not null,
 	level int default '0' not null,
 	`desc` varchar(256) default '' not null,
-	owner_id char(36) not null,
 	activity_id char(36) not null,
 	template_id char(36) not null,
-	class int default '0' not null,
-	kind int default '0' not null,
-	product_id varchar(36) default '' not null,
-	min_amount decimal(10,2) default '0.00' not null,
-	offer_amount decimal(10,2) default '0.00' not null,
-	used_count int default '0' not null,
-	used_time datetime default CURRENT_TIMESTAMP not null,
 	dead_time datetime default CURRENT_TIMESTAMP not null,
-	constraint cp_coupon_id_uindex
+	constraint cp_coupon_pack_id_uindex
 		unique (id)
 )
 ;
@@ -67,19 +100,3 @@ create table coupondb.cp_coupon_template
 )
 ;
 
-create table coupondb.cp_coupon_pack
-(
-	id char(36) not null,
-	post_time datetime default CURRENT_TIMESTAMP not null,
-	put_time datetime default CURRENT_TIMESTAMP not null,
-	name varchar(32) default '' not null,
-	active tinyint(1) default '1' not null,
-	level int default '0' not null,
-	`desc` varchar(256) default '' not null,
-	activity_id char(36) not null,
-	template_id char(36) not null,
-	dead_time datetime default CURRENT_TIMESTAMP not null,
-	constraint cp_coupon_pack_id_uindex
-		unique (id)
-)
-;
